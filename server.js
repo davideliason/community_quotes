053fbec593c767882eccef9dbdbd6a3b10f01e92
David Eliason <davethemaker@gmail.com>
2018-09-26 12:40:37 -0700

    console.log req.paramas id from url

--- server.js
+++ server.js
@@ -48,7 +48,11 @@ MongoClient.connect(uri, (err, database) => {
 	app.post('/quote', (req,res) => {
 		console.log("new quote posted");
 		db.collection('quotes').insertOne({"name" : req.body.name, "quote" : req.body.quote});
-	})
+	});
+
+	app.get('/quote/:id', (req,res) => {
+		console.log(req.params.id);
+	});
 
 	app.listen(port);
 });
