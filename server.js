6b437ae79c3a7c94d856c3ee32393f4967d7b432
David Eliason <davethemaker@gmail.com>
2018-09-26 11:00:29 -0700

    successfully connected to local mongodb instance. Added a data object using the mongo shell, and this object was successfully retrieved by the application and rendered to the browser by acting as a RESTful API

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const path = require('path');
 const logger = require('morgan');
+
 const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+
 const app = express();
+
 const port = process.env.PORT || 3000;
+const uri = process.env.DB_LOCAL_URI;
+
+//middleware
+
+MongoClient.connect(uri, function(err, client) {
+
+	db = client.db('todos2');
 
 app.get('/', (req,res) => {
-    res.end('hello world' + process.env.TEST);
+   db.collection('mytodos').find().toArray((err,data) => {
+   	// added data object to collection and this is rendered in view
+   	res.send(data);
+   })
 });
 
 app.listen(port, () => {
 	console.log("listening at 3000");
 })
+
+});
